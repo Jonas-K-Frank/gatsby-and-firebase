f45dd5dd1ade339f00d2d3b6700d6d7d81f65362
JonasFrank <Jonas@jkfrank.dk>
2020-09-20 11:39:51 +0200

    late commit

---
+++ Bourbon-club/src/components/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDH2AGqN6gTm0yVvYhLgxOPF6cqC6NM-4w",
+    authDomain: "gatsby-album-club.firebaseapp.com",
+    databaseURL: "https://gatsby-album-club.firebaseio.com",
+    projectId: "gatsby-album-club",
+    storageBucket: "gatsby-album-club.appspot.com",
+    messagingSenderId: "489099976955",
+    appId: "1:489099976955:web:2b14c0e7557189e2a0505e"
+  };
+
+  export default firebaseConfig;
---
+++ Bourbon-club/src/pages/login.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react"
+import { Link } from "gatsby"
+import {useAuth} from '../components/firebase'
+
+import Layout from "../components/layout"
+//import SEO from "../components/seo"
+
+const SecondPage = () => {
+
+    const [formValues, setFormValues] = useState({email: '', password: ''});
+    const {firebase} = useAuth();
+
+    function handleSubmit (event){
+        event.preventDefault();
+
+        firebase.login({email: formValues.email, password: formValues.password});
+    }
+
+    function handleInputChange(event) {
+        event.persist();
+        setFormValues(currentValues => ({
+            ...currentValues,
+            [event.target.name]: event.target.value
+        }))
+    }
+    return (
+  <Layout>
+      <form onSubmit={handleSubmit}>
+          <input value={formValues.email} name="name" onChange={handleInputChange}placeholder="email" type="email" />
+          <input value={formValues.password} name="password" onChange={handleInputChange} placeholder="password" type="password" />
+          <button type="submit">
+              Login
+          </button>
+      </form>
+  </Layout>
+)
+}
+
+export default SecondPage
